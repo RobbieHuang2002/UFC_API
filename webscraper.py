d65f5bba6fded69142ae3af8c082e99e23e9621c
RobbieHuang2002 <Robbie.huang2002@gmail.com>
2023-12-21 14:11:38 -0500

    uploaded knockout, submissions, first round knockout into a json

--- webscraper.py
+++ webscraper.py
@@ -1,18 +1,30 @@
 from bs4 import BeautifulSoup
+import lxml
 import requests
-
-# url = 'https://www.ufc.com'
-
-# page = requests.get(url)
-
-# soup = BeautifulSoup(page.text, features='html.parser')
+import json
 
 
-def search_fighter(fighterFirstName, fighterLastName):
+def get_fighter_stats(fighterFirstName, fighterLastName):
     url = f'https://www.ufc.com/athlete/{fighterFirstName}-{fighterLastName}'
     page = requests.get(url)
-    soup = BeautifulSoup(page.text,'html.parser')
-    soup.find_all('p')
-    print(soup)
-search_fighter('sean', 'strickland')
+    content = page.text
+    soup = BeautifulSoup(content, 'lxml')
+    stats = soup.find_all('div', class_="athlete-stats__stat")
+    stats_list = []
+    for i in stats:
+        stats_list.append(i.get_text())
+    print(stats_list)
+
+    fighter_stats = {
+        'knockouts': stats_list[0].replace("\n", " "),
+        'submissions':stats_list[1].replace("\n", " "),
+        'firstRoundKnockouts': stats_list[2].replace("\n", " ")
+    }
+
+    print(fighter_stats)
+
+
+
+
+get_fighter_stats('sean', 'strickland')
 
