e24dd28f01600978c624b4e7f6f47c6c5dd6f6c7
RobbieHuang2002 <robbie.huang2002@gmail.com>
2023-12-20 19:53:25 -0500

    added function for searching fighter

--- webscraper.py
+++ webscraper.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.ufc.com'
+# url = 'https://www.ufc.com'
 
-page = requests.get(url)
+# page = requests.get(url)
 
-soup = BeautifulSoup(page.text, features='html.parser')
+# soup = BeautifulSoup(page.text, features='html.parser')
 
-print(soup)
+
+def search_fighter(fighterFirstName, fighterLastName):
+    url = f'https://www.ufc.com/athlete/{fighterFirstName}-{fighterLastName}'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text,'html.parser')
+    soup.find_all('p')
+    print(soup)
+search_fighter('sean', 'strickland')
 
