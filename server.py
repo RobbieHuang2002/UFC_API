cfee061ed5d3dc4214dfe4e70af08694a661652b
RobbieHuang2002 <Robbie.huang2002@gmail.com>
2023-12-28 14:49:10 -0500

    changed filed name

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask
-import webscraper
+import fighter
 app = Flask(__name__)
 
 @app.route('/return_fighter_stats/<firstname>/<lastname>', methods=['GET'])
 def return_fighter_stats(firstname, lastname):
-    return  webscraper.get_fighter_stats(firstname, lastname)
+    return  fighter.get_fighter_stats(firstname, lastname)
 
 if __name__ == '__main__':
     app.run(debug=True)
