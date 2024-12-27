64c2c08b17a752a3298005ea829cf157c1153e89
RobbieHuang2002 <robbie.huang2002@gmail.com>
2024-12-27 00:24:14 -0500

    able to scrape all active fighter names

--- all_active_fighter.py
+++ all_active_fighter.py
@@ -9,24 +9,44 @@ class ActiveFighterNameExtractor:
     def __init__(self):
         pass
 
+
+    def split_name(self, name):
+        cleaned_name = " ".join(name.split())
+
+        parts = cleaned_name.split()
+
+        if len(parts) == 1:
+            return parts[0], "", ""
+        elif len(parts) == 2:
+            return parts[0], "", parts[1]
+        else:
+            return parts[0], " ".join(parts[1:-1]), parts[-1]
+            
     def extractName(self):
         counter = 1
-
-        url = "https://www.ufc.com/athletes/all?filters%5B0%5D=status%3A23" 
-
-        response = requests.get(url, params={"page": counter})
-
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        items = soup.find_all("span", class_="c-listing-athlete__name")
-        for item in items:
-            print(item)
-        
-
-        # page = requests.get(url)
-        # content=page.text
-        # soup = BeautifulSoup(content, 'lxml')
-        # names = soup.find_all(class_='c-listing-athlete__name')
+        result = []
+        previous_data = None
+        while True:
+            print(f"Fetching page {counter}...")
+            url = 'https://www.ufc.com/athletes/all'
+            response = requests.get(url, params={"page": counter, "filters[0]": "status:23"})
             
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            fighters = soup.find_all("div", class_="c-listing-athlete-flipcard white")
+            if fighters == previous_data:
+                print("no more new data")
+                break
+
+            for fighter in fighters:
+                fighter_name = fighter.find('span', class_="c-listing-athlete__name")
+                firstname, middlename, lastname = ActiveFighterNameExtractor().split_name(fighter_name.get_text())
+                result.append([firstname, middlename, lastname])
+                
+
+            counter += 1
+            previous_data = fighters
+        print(len(result))
+        return(result)
 if __name__ == '__main__':
     print(ActiveFighterNameExtractor().extractName())
